81ff697a0df53eee36e99877cbf6da584b078179
Abdulla Suhail <abdulla.suhail@gmail.com>
2021-11-14 11:59:04 +0530

    updated testRunner

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, stepNotifications = true, tags = "@Smoke", plugin = {
+@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, stepNotifications = true, plugin = {
 		"pretty", "html:target/cucumber.html", "json:target/cucumber.json", "junit:target/cukes.xml" })
 public class TestRunner {
 
