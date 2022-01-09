4085707a5353e8d3055031d940ba3bb006c8075f
Abdulla Suhail <abdulla.suhail@gmail.com>
2022-01-09 21:29:01 +0530

    hey

--- src/test/java/cucumberOptions/TestRunnerRegression.java
+++ src/test/java/cucumberOptions/TestRunnerRegression.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features/Practice.feature", glue = "stepDefinitions", monochrome = true, tags = "@Regression", stepNotifications = true, plugin = {
-		"pretty", "html:target/cucumber.html", "json:target/cucumber.json", "junit:target/cukes.xml" })
+		"pretty", "html:target/cucumber.html", "json:target/cucumber.json", "junit:target/RegressionTests.xml" })
 public class TestRunnerRegression {
 
 }
